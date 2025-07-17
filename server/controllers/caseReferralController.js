4866ce8c35d751f2bb7f5769e198c66ba63f1fa7
Ruhullah <ruhullah517@gmail.com>
2025-07-17 14:01:42 +0500

    22

--- server/controllers/caseReferralController.js
+++ server/controllers/caseReferralController.js
@@ -46,30 +46,41 @@ function transformWebsiteReferral(data) {
 // Map incoming WordPress field keys to backend field names
 function mapWordpressFields(data) {
   return {
-    referrerName: data['fields.name.raw_value'], // If available, else set manually
-    position: data['fields.field_d9861e8.raw_value'],
-    organization: data['fields.field_cc2e080.raw_value'],
-    contactNumber: data['fields.field_6834ad2.raw_value'],
-    emailAddress: data['fields.field_7e24913.raw_value'],
-    serviceType: data['fields.field_423164f.raw_value'],
-    hours: data['fields.field_516bfb9.raw_value'],
-    referralDetails: data['fields.field_8273cb2.raw_value'],
-    carerName: data['fields.name.raw_value'],
-    carerContactNumber: data['fields.field_2a317f5.raw_value'],
-    carerEmail: data['fields.email.raw_value'],
-    carerType: data['fields.field_74a621c.raw_value'],
-    sswName: data['fields.field_8dba340.raw_value'],
-    sswContactNumber: data['fields.field_6dbbe13.raw_value'],
-    sswEmail: data['fields.field_fa8987e.raw_value'],
-    sswLocalAuthority: data['fields.field_d51e22d.raw_value'],
-    decisionMakerName: data['fields.field_0640e53.raw_value'],
-    decisionMakerContactNumber: data['fields.field_6834ad2.raw_value'],
-    decisionMakerEmail: data['fields.field_7e24913.raw_value'],
-    financeContactName: data['fields.field_50fcec2.raw_value'],
-    financeContactNumber: data['fields.field_8cff087.raw_value'],
-    financeEmail: data['fields.field_a52aea1.raw_value'],
-    otherConsideration: data['fields.field_600bb55.raw_value'],
-    address: data['fields.field_71586e4.raw_value'],
+    // Referrer Details
+    referrerName: data['fields.name.raw_value'], // Referrer Name
+    position: data['fields.field_d9861e8.raw_value'], // Position
+    organization: data['fields.field_cc2e080.raw_value'], // Organisation
+    contactNumber: data['fields.field_2a317f5.raw_value'], // Referrer Contact Number (ends with 11)
+    emailAddress: data['fields.field_236cc38.raw_value'], // Referrer Email Address
+    serviceType: data['fields.field_423164f.raw_value'], // Service Type
+    hours: data['fields.field_516bfb9.raw_value'], // Hours
+    referralDetails: data['fields.field_8273cb2.raw_value'], // Detail of Referral
+
+    // Carer Details
+    carerName: data['fields.field_8dba340.raw_value'], // Carer's Name
+    carerContactNumber: data['fields.field_6dbbe13.raw_value'], // Carer Contact Number (ends with 22)
+    carerEmail: data['fields.field_fa8987e.raw_value'], // Carer Email Address
+    carerType: data['fields.field_74a621c.raw_value'], // Carer Type
+
+    // SSW
+    sswName: data['fields.field_600bb55.raw_value'], // SSW Name
+    sswContactNumber: data['fields.field_6834ad2.raw_value'], // SSW Contact Number (ends with 33)
+    sswEmail: data['fields.field_7e24913.raw_value'], // SSW Email
+    sswLocalAuthority: data['fields.field_d51e22d.raw_value'], // SSW Local Authority
+
+    // Decision Maker
+    decisionMakerName: data['fields.field_0640e53.raw_value'], // Decision Maker's Name
+    decisionMakerContactNumber: data['fields.field_8cff087.raw_value'], // Decision Maker's Contact Number (ends with 44)
+    decisionMakerEmail: data['fields.field_a52aea1.raw_value'], // Decision Maker's Email Address
+
+    // Finance Contact
+    financeContactName: data['fields.field_50fcec2.raw_value'], // Finance Name
+    financeContactNumber: data['fields.field_cc2e080.raw_value'], // Finance Contact Number (ends with 55)
+    financeEmail: data['fields.email.raw_value'], // Finance Email Address
+
+    // Additional Info
+    otherConsideration: data['fields.field_71586e4.raw_value'], // Other Consideration
+    address: data['fields.field_600bb55.raw_value'], // Address (if present)
   };
 }
 
