a1e40a4316c8f7567feaf44dd84b8703316d0b97
Ruhullah <ruhullah517@gmail.com>
2025-07-18 10:55:15 +0500

    30

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // Set up storage engine
 const storage = multer.diskStorage({
-  destination: './uploads/',
+  destination: '../uploads/',
   filename: function(req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   }
