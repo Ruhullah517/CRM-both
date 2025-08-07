50e92df715bdd8c694ef1584b2c4a4aee99a933c
Ruhullah <ruhullah517@gmail.com>
2025-08-07 13:07:29 +0500

    extend json limit so image can be send in email

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Make uploads folder static
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
