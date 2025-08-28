7e9d25439f399b9e16530d7c40658c27e9d2779c
Ruhullah <ruhullah517@gmail.com>
2025-08-28 13:31:47 +0500

    chnaged to outllok 365

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -7,17 +7,16 @@ const nodemailer = require('nodemailer');
 let cachedTransporter = null;
 
 function createTransporter() {
-    const host = 'smtp.gmail.com';
-    const port = 465; // ✅ Correct SSL port
-    const secure = true; // SSL
-    const user = 'ruhullah517@gmail.com';
-    const pass = 'jmxe agai wemf hqyy'; // ✅ Your email password
+    const service = 'Outlook365';
+    const port = 587; // ✅ Correct SSL port
+    const secure = false; // SSL
+    const user = 'hello@blackfostercarersalliance.co.uk';
+    const pass = 'Tinytoe08'; // ✅ Your email password
 
     const transporter = nodemailer.createTransport({
-        host,
-        port,
-        secure, // true for port 465, false for 587
+        service,
         auth: { user, pass },
+
     });
 
     return transporter;
