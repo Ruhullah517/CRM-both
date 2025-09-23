ee82c569573eb83e2a0a776e841dfdd3826c6f13
Ruhullah <ruhullah517@gmail.com>
2025-09-23 15:54:11 +0500

    fixing build issues front

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -7,7 +7,7 @@ export default function Loader() {
         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
         <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
       </svg>
-      <div className="text-[#2EAB2C] font-semibold text-lg">Loading...</div>
+      <div className="text-[#2EAB2C] font-semibold text-lg">Loading....</div>
     </div>
   );
 } 
